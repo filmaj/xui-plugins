9480e6412bd2f838a3396262d31cdf217155f0ba
Fil Maj <filip.maj@nitobi.com>
2011-01-16 02:50:31 -0800

    syntax error fix :o

--- fx-helpers/fx-helpers.js
+++ fx-helpers/fx-helpers.js
@@ -2,7 +2,7 @@ xui.extend({
    fade:function(to, callback) {
        var target = 0;
        if (typeof to == 'string' && to == 'in') target = 1;
-       } else if (typeof to == 'number') target = to;
+       else if (typeof to == 'number') target = to;
        this.tween({opacity:target,duration:.2}, callback);
    } 
 });

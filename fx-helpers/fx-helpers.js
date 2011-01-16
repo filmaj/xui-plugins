ef7a498b2b06de501782395a46c3699e5c7990ab
Fil Maj <filip.maj@nitobi.com>
2011-01-16 02:48:19 -0800

    Fix and smaller footprint.

--- fx-helpers/fx-helpers.js
+++ fx-helpers/fx-helpers.js
@@ -1,9 +1,8 @@
 xui.extend({
    fade:function(to, callback) {
        var target = 0;
-       if (typeof to == 'string') {
-           if (to == 'in') target = 1;
-       } else if (to) target = 1;
-       this.tween({opacity:to,duration:.2}, callback);
+       if (typeof to == 'string' && to == 'in') target = 1;
+       } else if (typeof to == 'number') target = to;
+       this.tween({opacity:target,duration:.2}, callback);
    } 
 });
